f9f76fc86829b9792abc799c660b60780b2b2d6b
pdvkpchtt <kabirdanilov14@gmail.com>
2024-10-22 19:35:43 +0500

    just to show 2

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>This is main branch</p>
+        <p>This is main branchaswwws</p>
         <a
           className="App-link"
           href="https://reactjs.org"
